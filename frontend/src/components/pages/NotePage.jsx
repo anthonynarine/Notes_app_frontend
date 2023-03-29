a32d63d0f26bb0908eff9ffc974409a89c874051
anthonynarine <anarine83@gmail.com>
2023-03-29 05:32:15 -0400

    it's 5 am in the morning, i sent the last 2 days researching this functionality, update functionality complete, small victory

--- frontend/src/components/pages/NotePage.jsx
+++ frontend/src/components/pages/NotePage.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
 //icon
-import {TfiBackLeft } from "react-icons/tfi"
+import { TfiBackLeft } from "react-icons/tfi";
 
 const NotePage = () => {
-
   // id destructured with useParmas passed from the path with the id parameter in apps.js
   let { id } = useParams();
+  let navigate = useNavigate();
   let [note, setNote] = useState(null);
   // state to store incoming note from below get request
 
@@ -17,33 +17,49 @@ const NotePage = () => {
     const getNote = async () => {
       try {
         // a proxy url can be set for this
-        let response = axios.get(`http://127.0.0.1:8000/api/notes/${id}`);
-        setNote((await response).data);
+        let {data} = await axios.get(`http://127.0.0.1:8000/api/notes/${id}`);
+        // id destructured from above used in url via template literal in the url
+        setNote(data);
+        console.log(data.body)
       } catch (error) {}
     };
     getNote();
-  }, [id]);
+  }, []);
 
   //on change handler for editing notes.
-  const handleNoteChange = (event) => {
-    setNote({...note, "body": event.target.value })
-// spread operator saying we want to update the note object, specifically the body of the note
+  const handleNoteUpdate = (event) => {
+  // spread operator saying we want to update the note object, specifically the body of the note
+    setNote({note});
+    try {
+      axios.put(`http://127.0.0.1:8000/api/notes/${id}/`,{
+        body: event.target.value
+      });     
+    } catch (error) {
+      console.log(error)    
+    }    
   };
 
-  // const updateNote
+  let handleSubmit = () => {
+    handleNoteUpdate()
+    navigate("/")
+
+  }
 
   return (
     <div className="note">
       <div className="note-header">
-      {/* links back to home page */}
-        <Link to="/" >
+        {/* links back to home page */}
+        <Link to="/">
           <h3>
-            <TfiBackLeft /><span style={{fontSize:20}}>back</span>
+            <TfiBackLeft onClick={handleSubmit} />
+            <span style={{ fontSize: 20 }}>back</span>
           </h3>
         </Link>
-
       </div>
-      <textarea onChange={handleNoteChange} defaultValue={note?.body}></textarea>
+      <textarea
+        onChange={handleNoteUpdate}
+        defaultValue={note?.body}
+      ></textarea>
     </div>
   );
 };
